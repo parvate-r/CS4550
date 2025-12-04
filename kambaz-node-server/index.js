2c3e133859fa430376494301fb6d1dafcc4f0421
parvate-r <parvate.r@northeastern.edu>
2025-12-03 21:27:34 -0500

    attempted fix

--- kambaz-node-server/index.js
+++ kambaz-node-server/index.js
@@ -1,5 +1,7 @@
 import express from "express";
-import "./connection.js";  // ensures MongoDB connects
+import cors from "cors";
+import "./connection.js";
+
 import usersRoutes from "./routes/users.routes.js";
 import coursesRoutes from "./routes/courses.routes.js";
 import modulesRoutes from "./routes/modules.routes.js";
@@ -7,12 +9,12 @@ import enrollmentsRoutes from "./routes/enrollments.routes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/courses", coursesRoutes);
 app.use("/api/modules", modulesRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/enrollments", enrollmentsRoutes);
-
 
 app.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
